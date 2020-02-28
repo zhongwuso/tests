6957a31041e55fcac89db747b8478be5a2cb5673
zhangAoWei821@163.com <aowei>
2020-02-28 23:40:50 +0800

    JWT-study

---
+++ src/test/java/cust/aowei/jwtstudy/JwtStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package cust.aowei.jwtstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
